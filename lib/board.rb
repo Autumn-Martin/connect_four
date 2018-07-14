27d42dcdd42a0f9901e254a0f4b63edbd7be4c70
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-14 17:35:09 -0600

    add completed idea for organizing by slots

--- lib/board.rb
+++ lib/board.rb
@@ -13,32 +13,15 @@ class Board
       "row1" => [".",".",".",".",".",".","."]
     }
 
-
-    @print_board = print_board
-
+    @draw_board = draw_board
   end
-  def print_board
-    # board = slots[:row_letter].join.scan(/.{1,7}/) <<"\n" #=> ["ABCDEFG"]
-    layout = slots.map do |row|
-      row.join.scan(/.{1,7}/) <<"\n"
+
+  def draw_board
+    layout = slots.map do |row, value|
+      value.join.scan(/.{1,7}/) <<"\n"
     end
-    # board = slots[:row_letter].join #=> ABCDEFG
     puts layout.join
   end
-    #or
-    # columns organized by letters
-    # @slots = {
-    #   "G" => [".",".",".",".",".","."],
-    #   "F" => [".",".",".",".",".","."],
-    #   "E" => [".",".",".",".",".","."],
-    #   "D" => [".",".",".",".",".","."],
-    #   "C" => [".",".",".",".",".","."],
-    #   "B" => [".",".",".",".",".","."],
-    #   "A" => [".",".",".",".",".","."]
-    # }
-  # def print_board
-  #   row1 = slots["A"][0]+ slots["B"][]
-  #   print board
-  # end
+
 
 end
