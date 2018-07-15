ea4dbe4da3e4d476e563e093ebc0859d76138e61
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-15 16:22:51 -0600

    add method for running the fill slot method with user input

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -4,11 +4,12 @@ puts "Welcome! Let's play Connect Four!"
 board = Board.new
 
 puts "Which slot would you like to select? Enter A,B,C,D,E,F, or G. >"
-guess_input = gets.chomp.upcase
+input = gets.chomp.upcase
 
+board.change_slots(input)
 # if guess_input == "A"
 # if guess_input == "B"
 # take user input, check that column -> find.first slot with "."
   # -> change slot to "X"
 # implement the computer's turn
-  # pick random column letter -> same loop 
+  # pick random column letter -> same loop
