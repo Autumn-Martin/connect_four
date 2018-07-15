6b3a681a822cdc9f919b1ef5519ca75faee1537e
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-14 21:16:32 -0600

    add method for just getting input

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -2,3 +2,6 @@ require "./lib/board"
 
 puts "Welcome! Let's play Connect Four!"
 board = Board.new
+
+puts "Which slot would you like to select? Enter A,B,C,D,E,F, or G. >"
+guess_input = gets.chomp
