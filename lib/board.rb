723f1a0ada6ad7a7179e7bd0f2d0864e15f4bda0
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-15 13:30:15 -0600

    edit method for drawing the board if board is organized by columns

--- lib/board.rb
+++ lib/board.rb
@@ -4,41 +4,53 @@ class Board
 
   def initialize
     # by rows
+    # @slots = {
+    #   letter: ["A","B","C","D","E","F","G"],
+    #   "row6" => [".",".",".",".",".",".","."],
+    #   "row5" => [".",".",".",".",".",".","."],
+    #   "row4" => [".",".",".",".",".",".","."],
+    #   "row3" => [".",".",".",".",".",".","."],
+    #   "row2" => [".",".",".",".",".",".","."],
+    #   "row1" => [".",".",".",".",".",".","."]
+    # }
+
+    @column_names = ["A","B","C","D","E","F","G"]
+
     @slots = {
-      letter: ["A","B","C","D","E","F","G"],
-      "row6" => [".",".",".",".",".",".","."],
-      "row5" => [".",".",".",".",".",".","."],
-      "row4" => [".",".",".",".",".",".","."],
-      "row3" => [".",".",".",".",".",".","."],
-      "row2" => [".",".",".",".",".",".","."],
-      "row1" => [".",".",".",".",".",".","."]
+      "A" => [".",".",".",".",".","."], # left to right is bottom to top
+      "B" => [".",".",".",".",".","."],
+      "C" => [".",".",".",".",".","."],
+      "D" => [".",".",".",".",".","."],
+      "E" => [".",".",".",".",".","."],
+      "F" => [".",".",".",".",".","."],
+      "G" => [".",".",".",".",".","."]
     }
 
     @draw_board = draw_board
 
-    # @slides = {
-    #   "A" => [".",".",".",".",".","."], # left to right is bottom to top
-    #   "B" => [".",".",".",".",".","."],
-    #   "C" => [".",".",".",".",".","."],
-    #   "D" => [".",".",".",".",".","."],
-    #   "E" => [".",".",".",".",".","."],
-    #   "F" => [".",".",".",".",".","."],
-    #   "G" => [".",".",".",".",".","."]
-    # }
-
-    # @slides = {
-    #   slots[:letter][1] => slots["row6"][1] + slots["row5"][1],
-    #   slots[:letter][2] => slots["row6"][2] + slots["row5"][2]
-    # }
-
   end
 
   def draw_board
-    layout = slots.map do |row, value|
-      value.join.scan(/.{1,7}/) <<"\n"
+    slots.each do |letter, slot|
+      print letter
     end
-    puts layout.join
+    puts
+    count = 0
+    until count == 7
+      slots.each do |letter, slot|
+          print slot[count]
+      end
+      count += 1
+      puts
+    end
   end
+
+  # def draw_board
+  #   layout = slots.map do |row, value|
+  #     value.join.scan(/.{1,7}/) <<"\n"
+  #   end
+  #   puts layout.join
+  # end
 
 
   # def change_board
