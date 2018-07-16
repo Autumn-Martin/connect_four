326e7fa41da7b2dd3ace3119e206aff0e11f7ba2
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-16 14:16:15 -0600

    add test for if player has a name based on player input

--- test/player_test.rb
+++ test/player_test.rb
@@ -6,7 +6,15 @@ require "./lib/player"
 
 class PlayerTest < Minitest::Test
   def test_it_exists
-    player = Player.new
+    player = Player.new("Leia")
     assert_instance_of Player, player
   end
+
+  def test_it_has_a_name
+    puts "What is your name?"
+    name = gets.chomp
+    player = Player.new(name)
+    assert_equal "#{name}", player.name
+  end
+  
 end
