aa50a5b17c65dd74526dd81381471efdfd8c45a2
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-16 16:42:09 -0600

    add test for getting player input

--- test/player_test.rb
+++ test/player_test.rb
@@ -11,6 +11,7 @@ class PlayerTest < Minitest::Test
   end
 
   def test_it_has_a_name
+    skip
     puts "What is your name?"
     name = gets.chomp
     player = Player.new(name)
@@ -20,5 +21,11 @@ class PlayerTest < Minitest::Test
   def test_it_has_a_chip
     player = Player.new("Leia")
     assert_equal "X", player.chip
+  end
+
+  def test_it_gets_input
+    # enter A when prompted 
+    player = Player.new("Leia")
+    assert_equal "A", player.get_input
   end
 end
