4a54388d4ec21c9a330907388ee4d82046be4e38
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-18 15:59:25 -0600

    reorganize tests & add tests for board

--- test/board_test.rb
+++ test/board_test.rb
@@ -10,20 +10,14 @@ class BoardTest < Minitest::Test
     assert_instance_of Board, board
   end
 
-  def test_it_finds_open_slot
-    board = Board.new
-    input = "A"
-    assert_equal 0, board.find_open_slot(input)
-  end
-
-  def test_slot_open_returns_true_if_slot_is_available
+  def test_can_tell_if_column_is_available
     board = Board.new
     input = "A"
 
-    assert board.slot_open?(input)
+    assert board.column_open?(input)
   end
 
-  def test_slot_open_returns_false_if_slot_is_unavailable
+  def test_can_tell_if_column_is_unavailable
     board = Board.new
     input = "A"
     board.slots["A"][0] = "X"
@@ -33,24 +27,53 @@ class BoardTest < Minitest::Test
     board.slots["A"][4] = "X"
     board.slots["A"][5] = "X"
 
-    refute board.slot_open?(input)
+    refute board.column_open?(input)
   end
 
-  # def test_it_places_chip_in_open_slot
-  #   board = Board.new
-  #   input = "A"
-  #   open_slot_index = board.find_open_slot(input)
-  #   assert_equal "X", board.place_chip(input, open_slot_index, chip)
-  # end
+  def test_slots_can_be_filled # integration test
+    board = Board.new
+    input = "A"
+    chip = "X"
+
+    open_slot_index = board.find_open_slot(input)
+    board.place_chip(input, open_slot_index, chip)
+
+    assert_equal "X", board.fill_slot(input, chip)
+  end
+
+  def test_it_finds_first_available_open_slot
+    board = Board.new
+    input = "A"
+
+    assert_equal 0, board.find_open_slot(input)
+
+    board.slots["A"][0] = "X"
+    board.slots["A"][1] = "X"
+    board.slots["A"][2] = "X"
+    board.slots["A"][3] = "X"
+    board.slots["A"][4] = "."
+    board.slots["A"][5] = "."
+
+    assert_equal 4, board.find_open_slot(input)
+  end
+
+  def test_it_replaces_empty_slot_with_chip
+    board = Board.new
+    input = "A"
+    chip = "X"
+    open_slot_index = 0
+
+    assert_equal "X", board.place_chip(input, open_slot_index, chip)
+  end
 
   def test_it_has_a_top_row
     board = Board.new
     assert_equal [".",".",".",".",".",".","."], board.top_row
   end
 
-  def test_can_tell_if_top_row_is_full # split this up later
+  def test_can_tell_if_top_row_is_empty
     board = Board.new
-    # set each element equal to "." or "X" or "O" in test
+    # set top row elements equal to "." in test
     board.slots["A"][5] = "."
     board.slots["B"][5] = "."
     board.slots["C"][5] = "."
@@ -60,6 +83,9 @@ class BoardTest < Minitest::Test
     board.slots["G"][5] = "."
     assert board.top_row_empty?
 
+  def test_can_tell_if_top_row_is_full
+    board = Board.new
+    # set top row elements equal to "X" or "O"  in test
     board.slots["A"][5] = "X"
     board.slots["B"][5] = "X"
     board.slots["C"][5] = "X"
@@ -70,13 +96,21 @@ class BoardTest < Minitest::Test
     refute board.top_row_empty?
   end
 
-  def test_can_tell_four_in_a_row_horizontally
-    board = Board.new
-    board.slots["A"][0] = "X"
-    board.slots["B"][0] = "X"
-    board.slots["C"][0] = "X"
-    board.slots["D"][0] = "X"
-    assert board.horizontal?
+
+  # def test_for_new_fill_index
+  #   board = Board.new
+  #   assert_equal new_fill_index("A", )
+  # end
+
   end
+
+  # def test_can_tell_four_in_a_row_horizontally
+  #   board = Board.new
+  #   board.slots["A"][0] = "X"
+  #   board.slots["B"][0] = "X"
+  #   board.slots["C"][0] = "X"
+  #   board.slots["D"][0] = "X"
+  #   assert board.horizontal?
+  # end
 
 end
