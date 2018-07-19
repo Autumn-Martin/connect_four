916db08c04474ba03b67d23b6a0e32dabded352e
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-18 18:06:23 -0600

    add methods for column win

--- lib/board.rb
+++ lib/board.rb
@@ -95,16 +95,21 @@ class Board
     end.to_a
   end
 
-  def count_x_or_o(grouped_chips, chip) #=> 4 integer if there are 4 X's
-    grouped_chips[0][1].count(chip)
-  end
-
   def four_x_in_a_row(grouped_chips) # detecting 4 in a row
     grouped_chips.any? {|group| group[1].join.include? "XXXX" || "OOOO"}
   end
 
   def four_o_in_a_row(grouped_chips) # detecting 4 in a row
     grouped_chips.any? {|group| group[1].join.include? "OOOO"}
+  end
+
+  def check_win(input, chip)
+    grouped_chips = group_column(input)
+    if chip == "X"
+      four_x_in_a_row(grouped_chips)
+    elsif chip == "O"
+      four_o_in_a_row(grouped_chips)
+    end
   end
   # def horizontal?
   #   if @slots[new_fill_index]  + @slots[new_fill_index - 1] + @slots[new_fill_index - 2] + @slots[new_fill_index - 3] == "XXXX"
